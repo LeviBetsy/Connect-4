7710219d580f033765f2dbc9023280c24c29fd96
LeviBetsy <kle2@trinity.edu>
2023-04-30 10:35:38 -0500

    We did everything

--- MyStack.java
+++ MyStack.java
@@ -1,26 +1,30 @@
 import java.util.ArrayList;
 
 public class MyStack {
-	private ArrayList<Board> stackArrayList;
+	private ArrayList<char[][]> stackArrayList;
 	private int undos;
 
 	public MyStack(int s, int u) {
-		stackArrayList = new ArrayList<Board>(s);
+		stackArrayList = new ArrayList<char[][]>(s);
 		undos = u;
 	}
 
-	public void push(Board j) {
-		stackArrayList.add(j);
+	public void push(char[][] j) {
+		// char[][] newj = new char[j.length][j[0].length];
+		char[][] newj = new char[7][6];
+		// newj = j.clone();
+
+		stackArrayList.add(newj);
 		if (stackArrayList.size() > undos) {
 			stackArrayList.remove(0);
 		}
 	}
 
-	public Board pop() {
+	public char[][] pop() {
 		return stackArrayList.remove(stackArrayList.size() - 1);
 	}
 
-	public Board peek() {
+	public char[][] peek() {
 		return stackArrayList.get(stackArrayList.size() - 1);
 	}
 
@@ -29,7 +33,7 @@ public class MyStack {
 	}
 
 	public void print() {
-		for (Board x : stackArrayList) {
+		for (char[][] x : stackArrayList) {
 			System.out.print(x + " ");
 		}
 		System.out.println();
